057a6ad5a106cf9e90e3c237197eccd3ff4d21e7
Beyondofsoul <sernigames@gmail.com>
2025-01-10 13:04:50 +0300

    Pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,36 @@
+import styles from './styles.module.css';
+
+function Pagination({ totalPages, handleNextPage, handlePrevPage, handlePageClick, currentPage }) {
+  return (
+    <div className={styles.pagination}>
+      <button className={styles.arrow} disabled={currentPage <= 1} onClick={handlePrevPage}>
+        {'<'}
+      </button>
+
+      <div className={styles.list}>
+        {[...Array(totalPages)].map((_, index) => {
+          return (
+            <button
+              onClick={() => handlePageClick(index + 1)}
+              className={styles.pageNumber}
+              disabled={index + 1 === currentPage}
+              key={index}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+
+      <button
+        className={styles.arrow}
+        onClick={handleNextPage}
+        disabled={currentPage >= totalPages}
+      >
+        {'>'}
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
