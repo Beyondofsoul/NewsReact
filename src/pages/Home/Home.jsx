d7fc2f7ac18db3132f5912d1f58e6ff87fc3132c
Beyondofsoul <sernigames@gmail.com>
2025-01-10 11:00:03 +0300

    skeleton

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,15 +4,19 @@ import styles from './styles.module.css';
 import { getNews } from '../../api/apiNews';
 import { useState } from 'react';
 import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton/Skeleton';
 
 function Home() {
   const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsLoading(true);
         const response = await getNews();
         setNews(response.news);
+        setIsLoading(false);
       } catch (err) {
         console.log(err);
       }
@@ -21,8 +25,12 @@ function Home() {
   }, []);
   return (
     <main className={styles.main}>
-      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
-      <NewsList news={news} />
+      {news.length > 0 && !isLoading ? (
+        <NewsBanner item={news[0]} />
+      ) : (
+        <Skeleton count={1} type={'banner'} />
+      )}
+      {!isLoading ? <NewsList news={news} /> : <Skeleton count={10} type={'item'} />}
     </main>
   );
 }
